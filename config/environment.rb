01b6194cae4d30ef7e26c8ea53b652747fc7670a
pingu <qpingu@gmail.com>
2011-09-03 12:40:30 -0700

    Enabled (optional) Heroku Sendgrid support

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,12 @@ Rails::Initializer.run do |config|
   require 'yaml'
   CONFIG = (YAML.load_file('config/config.yml')[RAILS_ENV] rescue {}).merge(ENV)
   CONFIG['s3'] = true if CONFIG['s3_access_id'] && CONFIG['s3_secret_key'] && CONFIG['s3_bucket_name']
+
+  # enable (optional) sendgrid support
+  CONFIG['smtp_user_name'] ||= CONFIG['SENDGRID_USERNAME']
+  CONFIG['smtp_password'] ||= CONFIG['SENDGRID_PASSWORD']
+  CONFIG['smtp_domain'] ||= CONFIG['SENDGRID_DOMAIN']
+
   config.time_zone = 'UTC'
   config.i18n.default_locale = :en
   config.active_record.partial_updates = true
